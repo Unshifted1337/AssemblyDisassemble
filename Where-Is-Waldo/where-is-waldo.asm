a9a88a10e1308dfabb24791eb2520d68461db7c3
Unshiftx86 <73394656+Unshifted1337@users.noreply.github.com>
2021-06-04 08:05:56 -0400

    Create where-is-waldo.asm

---
+++ Where-Is-Waldo/where-is-waldo.asm
@@ -0,0 +1,8 @@
+00000000004007f6 <identity>:
+  4007f6:       55                      push   %rbp
+  4007f7:       48 89 e5                mov    %rsp,%rbp
+  4007fa:       89 7d fc                mov    %edi,-0x4(%rbp)
+  4007fd:       8b 45 fc                mov    -0x4(%rbp),%eax
+           ***
+  400800:       5d                      pop    %rbp
+  400801:       c3                      retq
